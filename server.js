aba7972db5fbd995d7fff12d02e8c1dd9646ce10
abhisha shah <abhisha5993@gmail.com>
2020-01-01 17:04:18 -0500

    Make changes to deploy  project on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -20,8 +21,7 @@ const dbURL =  "mongodb://localhost:27017/mern-auth";
 
 //connect to MongoDB
 mongoose
-    .connect(
-    dbURL,
+    .connect(process.env.MONGODB_URI || dbURL,
     { useUnifiedTopology:true, useNewUrlParser: true }
     )
     .then(() => console.log("MongoDB successfully connected"))
@@ -35,6 +35,14 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*',(req, res) => {
+      res.sendFile(path.join(__dirname,'client','build','index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
