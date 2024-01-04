e062ec45958cfd32838c50b0e56e815e3694c941
devlopersabbir <devlopersabbir@gmail.com>
2024-01-05 00:13:14 +0600

    [feat 📌] mongodb connections

---
+++ config/connectDB.js
@@ -0,0 +1,19 @@
+const { connect } = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+/**
+ * @param {string} dbURL Database connection url
+ */
+export const connectDB = async (dbURL) => {
+  try {
+    await connect(process.env.MONGODB_URI || dbURL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("MongoDB successfully connected");
+  } catch (err) {
+    console.log("Fail to connect DB");
+    console.log(err);
+  }
+};
--- server.js
+++ server.js
@@ -1,31 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
 
 const users = require("./routes/api/users");
+const { connectDB } = require("./config/connectDB");
 
 const app = express();
 
 // Bodyparser middleware
 app.use(
-    bodyParser.urlencoded({
-      extended: false
-    })
-  );
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(bodyParser.json());
 
-const dbURL =  "mongodb://localhost:27017/mern-auth";
+const dbURL = "mongodb://localhost:27017/mern-auth";
 
-//connect to MongoDB
-mongoose
-    .connect(process.env.MONGODB_URI || dbURL,
-    { useUnifiedTopology:true, useNewUrlParser: true }
-    )
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
+connectDB(dbURL);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -36,16 +30,14 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-if(process.env.NODE_ENV === 'production') {
-   
-  app.use(express.static(path.join(__dirname, "client", "build")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get('*',(req, res) => {
-      res.sendFile(path.join(__dirname,'client','build','index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
-
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>console.log(`Server up and running on port ${port}`));
+app.listen(port, () => console.log(`Server up and running on port ${port}`));
